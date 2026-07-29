30cc2d7567fa3f60049100742289c59d757958c1
Grady Coffey <gcoffey0744@floridapoly.edu>
2026-07-29 17:27:27 -0400

    Create Grady_Coffey_Prob3_Assignment5DeckOfCards.h

---
+++ Grady_Coffey_Prob3_Assignment5DeckOfCards.h
@@ -0,0 +1,23 @@
+#ifndef DECKOFCARDS_H
+#define DECKOFCARDS_H
+
+#include <vector>
+#include "Card.h"
+
+class DeckOfCards
+{
+private:
+    std::vector<Card> deck;
+    int currentCard;
+
+public:
+    DeckOfCards();
+
+    void shuffle();
+
+    Card dealCard();
+
+    bool moreCards() const;
+};
+
+#endif
